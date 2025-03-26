ad3611453440511afe7371148eb2c5d573144c61
xGhostRider_ <tommasoquadrelli@gmail.com>
2025-03-26 11:50:18 +0100

    html

--- app/Main.py
+++ app/Main.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def Homepage():
-    return flask.render_template("Homepage.html")
+    return flask.render_template("homepage.html")
 
 @app.route("/login")
 def Login():
-    return flask.render_template("Login.html")
+    return flask.render_template("login.html")
+
+@app.route("/register")
+def Register():
+    return flask.render_template("register.html")
